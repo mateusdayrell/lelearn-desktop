9954198e407aad2e96f766ea81d7adea0a5648ec
mateusdayrell <mateusdayrell7@gmail.com>
2022-07-17 15:03:57 -0300

    feat: adicionar model de usuario

---
+++ src/model/UsuarioMODEL.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author mateu
+ */
+public class UsuarioMODEL {
+
+private String cpf;
+private String nome;
+private String telefone;
+private String senha;
+private String tipo;
+private String dataNasc;
+
+
+
+
+//CPF CHAR(11) PRIMARY KEY,
+//NOME VARCHAR(40) NOT NULL,
+//TELEFONE CHAR(11) NULL,
+//EMAIL VARCHAR(50) NOT NULL,
+//SENHA VARCHAR(15) NOT NULL,
+//TIPO VARCHAR(5) NOT NULL,
+//DATANASC DATE
+}
