5cf25607d4a0ad32d4e4735e72df3a6639a082be
mateusdayrell <mateusdayrell7@gmail.com>
2022-07-18 19:15:49 -0300

    feat: adicionar metodo cadastrar na classe UsuarioDAO

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -7,12 +7,20 @@ import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import jdbc.ConnectionFactory;
+import model.UsuarioMODEL;
 
 public class UsuarioDAO {
-    Connection conex;
+    private Connection conex;
     
+    // metodo construtor
+    public UsuarioDAO() {
+        this.conex = new ConnectionFactory().conectaBD();
+    }
+    
+    // método para autenticacao de Usuários
     public ResultSet autenticarUsuario(UsuarioDTO objUsuario) {
-        conex = new Conexao().conectaBD();
+        conex = new Conexao().conectaBD(); //REMOVER
         
         try {
             String sql = "select * from usuarios where email = ? and senha = ? ";
@@ -25,6 +33,34 @@ public class UsuarioDAO {
         } catch (SQLException error) {
             JOptionPane.showMessageDialog(null, "UsuarioDAO: " + error.getMessage());
             return null;
+        }
+    }
+    
+    //método para cadastro de usuários
+    public void cadastrarUsuario(UsuarioMODEL obj) {
+        try {
+            //criar o comando SQL
+            String sql = "insert into usuario (CPF, NOME, TELEFONE, EMAIL, SENHA, TIPO, DATANASC)"
+                        + "values (?, ?, ?, ?, ?, ?, ?)";
+            
+            //conectar BD e organizar comando SQL
+            PreparedStatement pstm = conex.prepareStatement(sql);
+            
+            //receber valores do Model (id da ?, valor)
+            pstm.setString(1, obj.getCpf());
+            pstm.setString(2, obj.getNome());
+            pstm.setString(4, obj.getTelefone());
+            pstm.setString(4, obj.getEmail());
+            pstm.setString(5, obj.getSenha());
+            pstm.setString(6, obj.getTipo());
+            pstm.setString(7, obj.getDataNasc());
+            
+            pstm.execute(); //executar comando
+            pstm.close();   //encerrar conexao
+            
+            JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar Usuário. \nUsuarioDAO: " + erro.getMessage());
         }
     }
 }
